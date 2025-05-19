522505fbf70b5f386deaf9d39f953bbbc9a874f2
arthur <arthur.wuhrmann@epfl.ch>
2025-05-19 22:23:21 +0200

    added PPO and DQN for scripts

---
+++ src/run/hyperparams_TD3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hyperparam_TD3_RL
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+#SBATCH --time=02:00:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+
+# Load any necessary modules
+module load gcc python openmpi py-torch 
+
+# Activate virtual environment if needed
+source .venv/bin/activate 
+
+# Run the script
+python3 src/models/TD3/hyperparam_search.py "$@"
